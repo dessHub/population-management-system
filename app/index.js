2854c7d2f5e39272f4428352c7067a61fa2f3307
desshub <korirdesmond@MacBook-Pro-3.local>
2019-05-29 12:04:27 +0300

    Add helmet to secure api

--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import 'dotenv/config';
 import logger from 'morgan';
 import bodyParser from "body-parser";
+import helmet from "helmet";
 
 import routes from './routes';
 
 const app = express();
 
+app.use(helmet())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
